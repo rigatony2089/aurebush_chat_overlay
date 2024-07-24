0d539509b63d0198c46d6471cc16c7dbbfacbb50
rigatony2089 <aventola@gmail.com>
2024-07-23 21:37:10 -0600

    Update script.js

--- script.js
+++ script.js
@@ -71,21 +71,19 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         }, 2000);  // 2 seconds delay
     }
-
+}
     function toAurebesh(text) {
         const aurebeshMap = {
-            'A': 'a', 'B': 'b', 'C': 'c', 'D': 'd', 'E': 'e',
+            'A': 'a', 'B': 'b', 'C': 'c', 'D': 'd', 'E': ‘e’,
             'F': 'f', 'G': 'g', 'H': 'h', 'I': 'i', 'J': 'j',
             'K': 'k', 'L': 'l', 'M': 'm', 'N': 'n', 'O': 'o',
             'P': 'p', 'Q': 'q', 'R': 'r', 'S': 's', 'T': 't',
             'U': 'u', 'V': 'v', 'W': 'w', 'X': 'x', 'Y': 'y', 'Z': 'z',
-            'a': 'a', 'b': 'b', 'c': 'c', 'd': 'd', 'e': 'e',
-            'f': 'f', 'g': 'g', 'h': 'h', 'i': 'i', 'j': 'j',
-            'k': 'k', 'l': 'l', 'm': 'm', 'n': 'n', 'o': 'o',
-            'p': 'p', 'q': 'q', 'r': 'r', 's': 's', 't': 't',
-            'u': 'u', 'v': 'v', 'w': 'w', 'x': 'x', 'y': 'y', 'z': 'z',
-            '0': '0', '1': '1', '2': '2', '3': '3', '4': '4',
-            '5': '5', '6': '6', '7': '7', '8': '8', '9': '9'
+	  ‘a’: 'a', ‘b’: 'b', ‘c’: 'c', ‘d’: 'd', ‘e’: ‘e’,
+            ‘f’: 'f', ‘g’: 'g', ‘h’: 'h', ‘i’: 'i', ‘j’: 'j',
+            ‘k’: 'k', ‘l’: 'l', ‘m’: 'm', ’n’: 'n', ‘o’: 'o',
+            ‘p’: 'p', ‘q’: 'q', ‘r’: 'r', ’s’: 's', ’t’: 't',
+            ‘u’: 'u', ‘v’: 'v', ‘w’: 'w', ‘x’: 'x', ‘y’: 'y', ‘z’: 'z',
         };
         return text.split('').map(char => aurebeshMap[char] || char).join('');
     }
